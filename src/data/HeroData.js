ba1f2726049f19b83e36aea11a116b9b36badd0e
Diego Insaurralde <djinsaurralde38@gmail.com>
2024-12-19 11:43:54 -0300

    🐶 Creativedog (Prueba Técnica)

--- src/data/HeroData.js
+++ src/data/HeroData.js
@@ -5,8 +5,8 @@ const heroData = [
     id: 1,
     theme: "dark",
     imgUrl: {
-        web: "https://s3-alpha-sig.figma.com/img/b70f/6376/4024554c2e141d6b2a710e601e7eb619?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mMoJeexSCY9nq1RJNO2jUGQZM4rhhkHeJ~3M2DW44zLLzsTpIjZmgh5MEo8iiJqcsu79aVtt-ffeqMHgYi2hhUMaibnD6E-QGAVmtllHCwbCTXkA1Pz7Jew7b7-me4Y5X6BtjzVGEuvkgAfctsvgSJ89KidBBsKFJwt9Hen6BR00EnHvzvHMgnynI0zgb5BhPuURTeUdmdmAaQSBapzto26v-gitV5G8E6CscACrPW-0z~~i9mv7mYrLzd1IltDkFbxr608gAx1doi8Fra90Ee6rM0rFUZ5W13z16XeiLBzSpt4kXrgvepis8e42BoO2mAsVjcyPJ2H~tt2p~Gzc5g__",
-        mobile: "https://s3-alpha-sig.figma.com/img/b70f/6376/4024554c2e141d6b2a710e601e7eb619?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NVIkDd27MxeFDxjgsHrFUORmCvKHqcxXm30G06S7PtxOc7PEByFuDmQtWWwpNGik~UueCeLqS7kwhABxppmWN6qWeHga6VrY9sf8tFBvj4UGMehXX0yCFTl4450GQjAzUBrThQCrjYCsNKaof3bQ6SQ9Ed82INxebNRFXNDyMebHYtdUWWQ1xtrStXuNq4Be4UkEbGnWEJc2VuDYtjCv2V1vem6fP06zFXpRSm~IXS3Tnaj6klIoa7VoFbIC3k0knCL8IY3b08pETErCqlp0YxpgWc8IXPG~rIXcDZkSRAyBaiysSZT7ObejPzUJbkNXWRooj5oPd84EC2ztU1m2Xg__",
+        web: "/assets/images/4024554c2e141d6b2a710e601e7eb619.jpg",
+        mobile: "/assets/images/4024554c2e141d6b2a710e601e7eb619.jpg",
     },
     title: "Conecta tu mundo con la tecnología más avanzada",
     description: "Explora nuestro catálogo de tecnología y encuentra todo lo que necesitas para estar conectado.",
@@ -26,8 +26,8 @@ const heroData = [
     id: 2,
     theme: "bright",
     imgUrl: {
-        web: "https://s3-alpha-sig.figma.com/img/4321/80cc/e68444b6c04c23af83ade2d32c1b5c5f?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mKb4PzBbupXLy2K5723QAl4HCZ6-3JfAZWnD8qTtvRLzqv1B0FZQFyMgULPQb~ZwmDAMXa9SwZ71fcJj~NfQNMwVjYbh~nM1bym4I1AlJp6D4nBy6kEn~BmdYacwxBbvDi1LCsg-PVDicEw-5u4JiCc8rmXZYZcbm49vMQh3ns6CRBjqjVX0rkBlbJIhkGU43WsbrCGhPc73v7Dyq7dywm1wlrFGuLhKZW2em1vVoP3ZH2lC1W~bsMuk~Z8DKAMY6ro1LAu0qflNn0F-3-xKsLT320~rprK6P~STCJXEtypzchRql~m07C95nE1X6U1cIp0SYyydhvuaZU9vkbvi~A__",
-        mobile: "https://s3-alpha-sig.figma.com/img/b4b2/c4d3/00e599fa2f9cdb87d9b6430e04b6e0d6?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dL5q2q7liQ8zmKASeo4hiNPlPg~rbAcn4rjP2r30wOMyrXv-nytnlZtRFQIk1SCiS5GNIgOkXriDSSflVM4YWfTpfWOhRPnCRKxswRxKn0T6IjxyF38-EWQObfxAO3WrRsZpxRwJqIjSWlZtrXyVOkN5NQCpBImF7FppN6Fu5kP4MA7AjCy4OOUrctW0QqVcHcHSPV04lJgSXxUc8vDIww9UHNhVsA7NYh6DQqxbj3J8MInzbKODPI-akb8vdJEh8Ypy3FsZQKDDb8LjIwcAL4ypxqvbsuMdcEfuL-NQxV2UuVq1S~5B4dlP3216iacubvtQsPL-yBkYeWZXWia67A__",
+        web: "/assets/images/e68444b6c04c23af83ade2d32c1b5c5f.jpg",
+        mobile: "/assets/images/00e599fa2f9cdb87d9b6430e04b6e0d6.jpg",
     },
     title: "Conecta tu mundo con la tecnología más avanzada",
     description: "Explora nuestro catálogo de tecnología y encuentra todo lo que necesitas para estar conectado.",
@@ -47,8 +47,8 @@ const heroData = [
     id: 3,
     theme: "dark",
     imgUrl: {
-        web: "https://s3-alpha-sig.figma.com/img/8bb2/a8f0/73497be66c83fc3f82e3765c308baf9c?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ST4wQYO08OZIUVTiM7jIDQWbx-mST4YVhxpQehxLL4Cu2MGkkLlBWvVlNk5AKe7AkqCA0ou6ZUkaAYlhz6BPLvkf22vJC82Y5Zx0O~g-8g1XS8-W4mH77ZGrHPwHNiiAPDylR12xYYXD8XMTogYsUtIz6BRe3h5yED7icc3FtY4OaPJ~CSCebvqXq8sRiC42gGT2Bk1c3YcsxNWhY2IejlOnCZZKY00KPjmQq6U27uMF-V2UXNAEywd5O2rGWzVKZ-bw3~27l71h6BR9GdIrP9DUW45Tb~gCH~rmOckNI89NcvWoXyRhRSo5OpEhKBwhM1Xu2N46RUn59L0RArMN5A__",
-        mobile: "https://s3-alpha-sig.figma.com/img/24df/b051/4ffef163d03b3c3e9b349dd75c4e944c?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BoVZWEm-jwkYdgpBN6dryB3BLQt7ufRnbXCmQ6Qi17yLbZCAjUP1bfuHw5SX2rtxQ~3fDm3GS7yTtdq0Yyg0qbDyUHBEXy2zUcbqmaEANBn83qic9tcXijN98fT2oAsQlxe23jvaOvrpGtlMboY0J-wLoL2zoTpzw66Ox0aNNa1PLnrEyfjiQc9b4jcgO7QgUrdCauyOYWXxDYnkhLhQrYrEktpyfSrn2J7cJsFcwaHlczbTrxAHXBpc1SYhN~9QoFVQuHRvOCWbNbUjhk~0e67rCxtkS~v1ncvurm0hRzMtcxzoExHAR2eiEYDaKLcLPrar2g0yskoWSKTvFA9xUg__",
+        web: "/assets/images/73497be66c83fc3f82e3765c308baf9c.jpg",
+        mobile: "/assets/images/4ffef163d03b3c3e9b349dd75c4e944c.jpg",
     },
     title: "Conecta tu mundo con la tecnología más avanzada",
     description: "Explora nuestro catálogo de tecnología y encuentra todo lo que necesitas para estar conectado.",
