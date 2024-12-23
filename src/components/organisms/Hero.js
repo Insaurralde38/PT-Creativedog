b9f8df45665ad6a4218d9aee7a4fcb6a5804bca1
Diego Insaurralde <djinsaurralde38@gmail.com>
2024-12-23 17:25:53 -0300

    🐶 Creativedog (Prueba Técnica)

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -1,54 +1,4 @@
-// import React, { useEffect } from "react";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination, Autoplay, EffectFade } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/effect-fade";
-// import styles from "@/globalStyles.module.css";
-// import heroData from "@/data/HeroData";
-// import HeroSlide from "@/components/molecules/HeroSlide";
-// import paginationPosition from "@/utils/paginationPosition"
-
-// const Hero = () => {
-
-//   useEffect(() => {
-//     paginationPosition(styles);
-//     window.addEventListener("resize", () => paginationPosition(styles));
-
-//     return () => {
-//       window.removeEventListener("resize", () => paginationPosition(styles));
-//     };
-//   }, []);
-
-//   return (
-//     <div id="hero-section" className={styles.heroContainer}>
-//       <Swiper
-//         modules={[Pagination, Autoplay, EffectFade]}
-//         slidesPerView={1}
-//         effect="fade"
-//         fadeEffect={{ crossFade: true }}
-//         pagination={{
-//           el:".swiper-pagination",
-//           clickable: true,
-//           type: "bullets",
-//           bulletClass: "swiper-custom-bullet",
-//           bulletActiveClass: "swiper-custom-bullet-active",
-//         }}
-//         autoplay={{ delay: 2000 }}
-//       >
-//         {heroData.map((item) => (
-//           <SwiperSlide key={item.id} className={styles.heroSlide}>
-//             <HeroSlide item={item}/>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//       <div className="swiper-pagination" />
-//     </div>
-//   );
-// };
-
-// export default Hero;
-import React from "react";
+import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
@@ -57,10 +7,20 @@ import "swiper/css/effect-fade";
 import styles from "@/globalStyles.module.css";
 import heroData from "@/data/HeroData";
 import HeroSlide from "@/components/molecules/HeroSlide";
-import useHeroPosition from "@/utils/useHeroPosition";
+import paginationPosition from "@/utils/paginationPosition";
 
 const Hero = () => {
-  useHeroPosition();
+  
+  useEffect(() => {
+    paginationPosition();
+    window.addEventListener("resize", paginationPosition);
+    window.addEventListener("scroll", paginationPosition);
+
+    return () => {
+      window.removeEventListener("resize", paginationPosition);
+      window.removeEventListener("scroll", paginationPosition);
+    };
+  }, []);
 
   return (
     <div id="hero-section" className={styles.heroContainer}>
---
+++ src/utils/paginationPosition.js
@@ -0,0 +1,22 @@
+import styles from "@/globalStyles.module.css"
+
+const paginationPosition = () => {
+  const heroContainer = document.getElementById("hero-section");
+  const heroInfo = heroContainer?.querySelector(`.${styles.heroInfo}`);
+  const pagination = heroContainer?.querySelector(".swiper-pagination");
+  const bullet = pagination?.querySelector(".swiper-custom-bullet");
+
+  if (heroContainer && heroInfo && pagination && bullet) {
+    const heroContainerRect = heroContainer.getBoundingClientRect();
+    const heroInfoRect = heroInfo.getBoundingClientRect();
+
+    const containerBottom = heroContainerRect.bottom;
+    const infoBottom = heroInfoRect.bottom;
+    const bulletHeight = bullet.offsetHeight || 0;
+
+    const distance = (containerBottom - infoBottom) * 0.7 - bulletHeight;
+    pagination.style.bottom = `${distance}px`;
+  }
+};
+
+export default paginationPosition;
