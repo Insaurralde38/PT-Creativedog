b44a386a05bffe92552a300234f98b0eaa250737
Diego Insaurralde <djinsaurralde38@gmail.com>
2024-12-23 16:18:50 -0300

    🐶 Creativedog (Prueba Técnica)

--- src/utils/useHeroPosition.js
+++ src/utils/useHeroPosition.js
@@ -1,21 +1,23 @@
 import { useEffect } from "react";
-import styles from "@/globalStyles.module.css"
+import styles from "@/globalStyles.module.css";
 
 const useHeroPosition = () => {
   useEffect(() => {
     const heroContainer = document.getElementById("hero-section");
     const heroInfo = heroContainer.querySelector(`.${styles.heroInfo}`);
     const pagination = heroContainer.querySelector(".swiper-pagination");
+    const bullet = pagination?.querySelector(".swiper-custom-bullet");
 
-    if (heroContainer && heroInfo && pagination) {
+    if (heroContainer && heroInfo && pagination && bullet) {
       const updatePaginationPosition = () => {
         const heroContainerRect = heroContainer.getBoundingClientRect();
         const heroInfoRect = heroInfo.getBoundingClientRect();
 
         const containerBottom = heroContainerRect.bottom;
         const infoBottom = heroInfoRect.bottom;
+        const bulletHeight = bullet.offsetHeight || 0;
 
-        const distance = (containerBottom - infoBottom) * 0.6;
+        const distance = (containerBottom - infoBottom) * 0.7 - bulletHeight;
 
         pagination.style.bottom = `${distance}px`;
       };
